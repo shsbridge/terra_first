9026aa12fe16c6390ab10b933e8d72ec2ab119df
Tom Wei <shsbridge@gmail.com>
2019-02-10 21:08:42 +0800

    add key

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "example" {
    ami = "ami-0c5199d385b432989"
    instance_type = "t2.micro"
    vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+   key_name = "EffectiveDevOpsAWS"
    user_data = <<-EOF
                #!/bin/bash
                echo "Hello, World, This is a test webpage" > index.html
@@ -25,5 +26,11 @@ resource "aws_security_group" "instance" {
     to_port   = 8080
     protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress{
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["121.7.188.193/32"]
   }
 }
