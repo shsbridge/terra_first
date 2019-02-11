6c1cad22b5e67dfd0f4b589a11451be15e7522c8
Tom Wei <shsbridge@gmail.com>
2019-02-11 22:47:12 +0800

    add elb

--- main.tf
+++ main.tf
@@ -1,22 +1,46 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
-resource "aws_autoscaling_group" "example"{
+resource "aws_elb" "example" {
+   name            = "terraform-asg-example"
+   availability_zones = ["${data.aws_availability_zones.all.names}"]
+   security_groups  = ["${aws_security_group.elb.id}"]
+   
+   listener {
+     lb_port   = 80
+     lb_protocol = "http"
+     instance_port = "${var.http_port}"
+     instance_protocol = "http"
+   }
+   
+   health_check {
+     healthy_threshold  = 2
+     unhealthy_threshold  = 2
+     timeout    = 3
+     interval   = 30
+     target     = "HTTP:${var.http_port}/"
+   }
+}
+resource "aws_autoscaling_group" "example" {
    launch_configuration = "${aws_launch_configuration.example.id}"
    availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+   load_balancers   = ["${aws_elb.example.name}"]
+   health_check_type  = "ELB"
+
    min_size = 2
    max_size = 4
 
    tag {
-     key       = "Name"
-     value     = "Terraform-ASG-example"
-     propagate_at_launch = ture
-   }
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 resource "aws_launch_configuration" "example" {
-   ami = "ami-0c5199d385b432989"
+   image_id  = "ami-0c5199d385b432989"
    instance_type = "t2.micro"
-   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+   security_groups = ["${aws_security_group.instance.id}"]
    key_name = "EffectiveDevOpsAWS"
    user_data = <<-EOF
                #!/bin/bash
@@ -47,11 +71,26 @@ resource "aws_security_group" "instance" {
     create_before_destroy = true
   } 
 }
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 variable "http_port" {
   description = "This is http service port"
   default = 8080
 }
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
-}
 data "aws_availability_zones" "all" {}
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
+}
